e9bf0ec24aaa0fcc63f1ce4c0e592b16a11defaf
ziadidi <dianepucci19@gmail.com>
2022-07-27 14:56:35 +0100

    almost there

--- index.js
+++ index.js
@@ -2,6 +2,32 @@
 // service_wgp8mrr
 // bKmr7S0aAPCuhkjGM
 
+let isModalOpen = false;
+let contrasToogle = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+    
+    for (let i = 0; i < shapes.length; i++) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+    }
+}
+
+function toggleContrast() {
+    contrasToogle = !contrasToogle;
+    if (contrasToogle) {
+        document.body.classList += " dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+}
+
 function contact(event) {
     event.preventDefault();
     const loading = document.querySelector('.modal__overlay--loading')
@@ -24,7 +50,6 @@ function contact(event) {
      })
 }
 
-let isModalOpen = false;
 function toggleModal() {
     if (isModalOpen) {
         isModalOpen = false;
